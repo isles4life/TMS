ebd81d4fc7d48aaf74e1233f595592a0986172e7
isles4life <isles4life@gmail.com>
2026-01-08 11:32:26 -0500

    Compliance & Safety: Core Infrastructure

--- backend/src/Infrastructure/obj/Debug/net10.0/TMS.Infrastructure.AssemblyInfo.cs
+++ backend/src/Infrastructure/obj/Debug/net10.0/TMS.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38855903225babf6e7edf30bc8cf836d5a356bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e466fca1b24eb72bbd0d8ddbe454d54521f219")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
